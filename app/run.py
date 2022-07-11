9235a2e0b25174f5f52c5206102851e89481365a
siavash.nejadi@gmail.com <siavash.nejadi@ucalgary.ca>
2022-07-11 12:42:12 -0600

    a block of code was commented out, fixed it

--- app/run.py
+++ app/run.py
@@ -142,9 +142,9 @@ def go():
     )
 
 
-# def main():
-    # app.run(host='0.0.0.0', port=3001, debug=True)
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
